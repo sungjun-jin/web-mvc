f842dff9d3a4831b6f94db4545f94856c87dad99
sungjun-jin <sungjun@peoplefund.co.kr>
2022-05-22 17:45:26 +0900

    feat: content type 추가

--- src/main/java/devel/study/webmvc/SampleController.java
+++ src/main/java/devel/study/webmvc/SampleController.java
@@ -1,5 +1,6 @@
 package devel.study.webmvc;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -7,7 +8,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class SampleController {
 
-    @GetMapping("/hello")
+    // Accept 헤더에 JSON TYPE 허용
+    @GetMapping(value = "/hello", consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public String hello() {
         return "hello";
--- src/test/java/devel/study/webmvc/SampleControllerTest.java
+++ src/test/java/devel/study/webmvc/SampleControllerTest.java
@@ -4,10 +4,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -20,7 +22,10 @@ public class SampleControllerTest {
 
     @Test
     public void helloTest() throws Exception {
-        mockMvc.perform(get("/hello"))
+        mockMvc.perform(get("/hello")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().string("hello"))
                 ;
