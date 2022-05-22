bae0079f08f9e32cc0215d83ee8bfed5d90f4645
sungjun-jin <sungjun@peoplefund.co.kr>
2022-05-22 17:09:00 +0900

    first commit

---
+++ src/test/java/devel/study/webmvc/WebMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package devel.study.webmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
